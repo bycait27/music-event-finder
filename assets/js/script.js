c1beca054dcfcd16f5effe3e2790ffe4f7f28f06
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-07-31 21:35:14 -0500

    adding audiodb api functionality, testing data on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
 const lightMode = document.getElementById('lightModeBtn');
 const darkMode = document.getElementById('darkModeBtn');
 
-//  fetch('rest.bandsintown.com/artists/{artistname}/events?=app_id={}')
-//     .then(function(response) {
-//         return response.json()
-//     })
-//     .then(function(data) {
-//         console.log(data.name);
 
-        
-//     })
+// Gather value for inputs
+// event listener    
 
-    fetch('https://theaudiodb.com/api/v1/json/2/artist.php?i=112024')
+const artistSearch = document.getElementById('artist-search');
+const searchBtn = document.getElementById('search-btn');
+const albumInfo = document.getElementById('albums');
+
+
+function fetchData() {
+    fetch(`https://theaudiodb.com/api/v1/json/2/discography.php?s=${artistSearch.value}`)
     .then(function(response) {
         return response.json()
     })
     .then(function(data) {
         console.log(data);
+       // document.getElementById('albums').innerText= data.album[0].strAlbum;
+      
     })
+    
+}
+ 
+searchBtn.addEventListener("click", fetchData);
+
+
+
  
   
 
